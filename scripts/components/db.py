49c623ab1d37e3e0debd53ba60b02b40a3cdf3e6
Abd3d <fanshawe3d@gmail.com>
2022-04-09 12:23:30 -0400

    Create Database file

---
+++ scripts/components/db.py
@@ -0,0 +1,32 @@
+mQuestions = {1: "Does your character wear a cape?",
+              2: "Do you have any robotic features? ",
+              3: "Can your character breathe underwater?",
+              4: "Does your character wear a mask? ",
+              5: "Does your superpower allow you to talk to fish? ",
+              6: "Does your character have x-ray vision?",
+              7: "Can your arm automatically turn into a deadly weapon?",
+              8: "Is your enemy the Joker?",
+              9: "Does the Green Kryptonite negatively affect you?",
+              10: "Are you the leader of the Justice league?",
+              11: "Is your character's superhero outfit black?",
+              12: "Is your character one of the main leads in the movie “Man Of Steel” ?",
+              13: "Does your character usually live on land?",
+              14: "Is your mother a queen?",
+              15: "Are you only half-human?",
+              16: "Can you talk to computers?",
+              17: "Can your character turn into a music boombox?",
+              18: "Was your character raised by dolphins?",
+              19: "Is your brother a villain?",
+              20: "Did your character adopt any kid?",
+              21: "Was your character's birthplace in one of the real life states in the US?",
+              22: "Was your character’s first debut in the 1940’s?",
+              23: "Was your character’s first debut in the 1930’s?",
+              24: "Is your superhero vehicle’s color black?",
+              25: "Is your character’s name also in the title of a movie of two Justice League character’s fighting each other? "
+              }
+mVar = [["b", "s"], ["c"], ["c", "a"], ["b"], ["a"], ["s"], ["c"], ["b"], ["s"], ["b"], ["b"], ["b", "s"], [
+    "a"], ["a"], ["c", "a"], ["c"], ["c"], ["a"], ["a"], ["b", "s"], ["c"], ["a"], ["b", "s"], ["b"], ["b", "s"]]
+mB = 0
+mS = 0
+mC = 0
+mA = 0
